0dea93238c15d3cf9f74022851d8e102f8e6b767
ABDULREHMAN <awanm6815@gmail.com>
2026-03-27 15:48:19 +0500

    Add files via upload

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Laravel\Passport\HasApiTokens;
+use App\Http\Controllers\Api\ApiController;
+
+//open routes
+Route::post("register", [ApiController::class, "register"]);
+Route::post("login", [ApiController::class, "login"]);
+
+//protected routes
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
+
+
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
